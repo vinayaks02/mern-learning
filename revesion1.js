e9c75842f170f4174ff91d61e23f05d9aa5dd606
Vinayak02 <vinayaksawant0202@gmail.com>
2026-06-16 21:04:05 +0530

    Day 1 revision complete

---
+++ revesion1.js
@@ -0,0 +1,17 @@
+/*const myName = `Vinayak Sawant`;
+const myCity = `Belgaum`;
+let myJob = `MERN Developer`;
+
+console.log(`My Name Is ${myName}`);
+console.log(`I live in ${myCity}`);
+console.log(`My Dream Job is ${myJob}`);
+
+let myAge = 35;
+let myExp = 10;
+let mySalary = 35000;
+let noticePeriod = 30;
+
+console.log(`I am ${myAge} old`);
+console.log(`I have ${myExp} years of Experence`);
+console.log(`My Expected Salary is ${mySalary}`);
+console.log(`My notice period is ${noticePeriod} days`);*/
