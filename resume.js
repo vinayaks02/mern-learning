a52a85222d24e4d1336c84444ffc3e9e57861671
Vinayak02 <vinayaksawant0202@gmail.com>
2026-06-17 22:56:58 +0530

    Created Resume using object

---
+++ resume.js
@@ -0,0 +1,13 @@
+const resume = {
+  name: "Vinayak Sawant",
+  age: 35,
+  city: "Belgaum",
+  jobgoal: "Web Developer",
+  skills: ["HTML", "CSS", "Javascript", "React"],
+  hobbies: ["Football", "Cricket", "Coding"],
+  isAvailable: true,
+};
+
+console.log(
+  `Hi, I am ${resume.name} from ${resume.city}, I am ${resume.age} years old, my job goal is to become ${resume.jobgoal} and my skills are ${resume.skills} and my hobbies are ${resume.hobbies} and I am available ${resume.isAvailable}`,
+);
