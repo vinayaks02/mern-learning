a3d236f2da0778dc1f5684c6f398685022355bfb
Vinayak02 <vinayaksawant0202@gmail.com>
2026-06-20 17:04:53 +0530

    Functions

---
+++ functions.js
@@ -0,0 +1,7 @@
+function info(name, city, job) {
+  console.log(`Hi I am ${name} from ${city} and I want to become ${job}`);
+}
+
+info("VInayak", "Belgaum", "Web Developer");
+info("Rahul", "Mumbai", "Technician");
+info("Kusum", "Belgaum", "Tailor");
