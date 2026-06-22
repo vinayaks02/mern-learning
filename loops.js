a993d3e096b59817525452260db81890cf5c597d
Vinayak02 <vinayaksawant0202@gmail.com>
2026-06-22 19:39:53 +0530

    Loop

---
+++ loops.js
@@ -0,0 +1,8 @@
+// for (let i = 1; i <= 5; i++) {
+//   console.log(`Floor ${i} Completed`);
+// }
+
+const myskills = ["HTML", "CSS", "JavaScript", "React"];
+for (i = 0; i < myskills.length; i++) {
+  console.log(`My Skills ${i + 1} is ${myskills[i]}`);
+}
