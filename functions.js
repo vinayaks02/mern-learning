5563f14c4ce1c3a38079bc8c662910e41c0ba66c
Vinayak02 <vinayaksawant0202@gmail.com>
2026-06-22 14:46:44 +0530

    Functions practise

--- functions.js
+++ functions.js
@@ -1,7 +1,35 @@
-function info(name, city, job) {
+/*function info(name, city, job) {
   console.log(`Hi I am ${name} from ${city} and I want to become ${job}`);
 }
 
 info("VInayak", "Belgaum", "Web Developer");
 info("Rahul", "Mumbai", "Technician");
 info("Kusum", "Belgaum", "Tailor");
+
+function makeChai() {
+  console.log("Chai is raedy");
+  return "Here is your chai";
+}
+
+const myChai = makeChai();
+console.log(myChai);
+
+function calculateSalary(basic, bonus) {
+  return basic + bonus;
+}
+const totalSalary = calculateSalary(25000, 5000);
+console.log(`My Total Salry is: ${totalSalary}`);*/
+
+function checkExperience(years) {
+  if (years >= 2) {
+    return "You are Selected";
+  } else {
+    return " You are not Selected";
+  }
+}
+
+const Rahul = checkExperience(3);
+console.log(Rahul);
+
+const Priya = checkExperience(1);
+console.log(Priya);
