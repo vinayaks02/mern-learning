d013a863c20387f4253620d49e5aa297db5f10b2
Vinayak02 <vinayaksawant0202@gmail.com>
2026-06-22 20:01:31 +0530

    for each loop

--- loops.js
+++ loops.js
@@ -6,7 +6,13 @@
 // for (i = 0; i < myskills.length; i++) {
 //   console.log(`My Skills ${i + 1} is ${myskills[i]}`);
 // }
-const mySKills = ["HTML", "CSS", "JavaScript", "React"];
-mySKills.forEach((skill, index) => {
-  console.log(`I Know ${index + 1} ${skill}`);
+// const mySKills = ["HTML", "CSS", "JavaScript", "React"];
+// mySKills.forEach((skill, index) => {
+//   console.log(`I Know ${index + 1} ${skill}`);
+// });
+
+const scores = [45, 67, 85, 34];
+
+scores.forEach((player, index) => {
+  console.log(`player ${index + 1} scored ${player} runs`);
 });
